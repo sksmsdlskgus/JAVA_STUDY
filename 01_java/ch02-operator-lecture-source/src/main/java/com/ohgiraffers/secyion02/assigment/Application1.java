357db8f17386dde6db9339494281becc39b7048d
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-07-11 10:25:13 +0900

    assigment

---
+++ 01_java/ch02-operator-lecture-source/src/main/java/com/ohgiraffers/secyion02/assigment/Application1.java
@@ -0,0 +1,38 @@
+package com.ohgiraffers.secyion02.assigment;
+
+public class Application1 {
+
+    /* 수업목표. 복합대입 연산자를 이해하고 활용할 수 있다. */
+    /*  필기.
+     *  대입연산자와 산술 복합 대입 연산자
+     *   '=', '+=', '-=','*=','/=','%='
+     */
+
+    public static void main(String[] args) {
+        int num = 12;
+        System.out.println("num = " + num);
+
+        num = num + 3;
+        System.out.println("num = " + num);
+
+        num += 3;
+        System.out.println("num = " + num);
+
+        num -=5;
+        System.out.println("num = " + num);
+
+        num *= 2;
+        System.out.println("num = " + num);
+
+        num /= 2;
+        System.out.println("num = " + num);
+
+        num %= 10;
+        System.out.println("num = " + num);
+
+        /* 주의 ! */
+        num =- 5;        //equal(=) 기호 오른쪽은 복합 대입 연산자가 아니다.
+        System.out.println("num = " + num);
+
+    }
+}
