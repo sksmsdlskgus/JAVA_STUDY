a2f93c5be900bddc8f5f3c1728f9bb4af538f4ae
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-07-15 12:29:03 +0900

    array refactor

--- 01_java/chap05-array-lecture-source/src/main/java/com/ohgiraffers/section03/copy/Application2.java
+++ 01_java/chap05-array-lecture-source/src/main/java/com/ohgiraffers/section03/copy/Application2.java
@@ -7,11 +7,11 @@ public class Application2 {
 
         /* 수업목표. 깊은 복사에 대해 이해할 수 있다. */
         /*  필기.
-         *   깊은 복사를 하는 방법은 4가지가 잇다.
+         *   깊은 복사를 하는 방법은 4가지가 있다.
          *    1. for문을 이용한 동일한 인덱스 값을 일일이 복사
          *    2. Object의 clone()을 이용한 복사(사용 빈도 놓은)
          *    3. System의 arraycopy()를 이용한 복사
-         *    4. Arrats의 copyOf()를 이용한 복사
+         *    4. Arrays의 copyOf()를 이용한 복사
          */
 
         int[] originArr = new int[] {1,2,3,4};
@@ -30,7 +30,7 @@ public class Application2 {
         print(copyArry2,"clone()을 활용한 사본");
 
         /* 목차. 3. arraycopy()를 이용한 복사 */
-        int[] copyArrr3 = new int[originArr.length + 3];  //원본봗 3크기가 더 큰 배열 할당
+        int[] copyArrr3 = new int[originArr.length + 3];  //원본보다 3크기가 더 큰 배열 할당
         System.arraycopy(originArr,0,copyArrr3,3,3);
         print(copyArrr3,"arraycopy()를 활용한 사본");
 
