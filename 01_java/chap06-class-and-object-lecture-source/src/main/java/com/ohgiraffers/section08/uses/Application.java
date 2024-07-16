19f6403c8926b82ca6b734d6e0436323ac03833d
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-07-16 16:03:52 +0900

    uses

---
+++ 01_java/chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section08/uses/Application.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.section08.uses;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        MemberService memberManager = new MemberService();
+
+        while(true){
+            System.out.println("========회원 관리 프로그램=======");
+            System.out.println("1. 회원 등록");
+            System.out.println("2. 회원 전체 조회");
+            System.out.println("9. 프로그램 종료");
+            System.out.println("메뉴 선택: ");
+            int mo = sc.nextInt();
+
+            switch(mo){
+                case 1: memberManager.signUpMember(); break;
+                case 2: memberManager.showAllMembers(); break;
+                case 9:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("잘못된 번호를 입력하셨습니다.");
+            }
+        }
+    }
+}
