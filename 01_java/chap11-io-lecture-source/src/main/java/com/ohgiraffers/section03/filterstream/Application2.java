4ab6729f1dc967e9789af894e534860cd8e48fcc
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-07-22 12:45:45 +0900

    Buffered2

---
+++ 01_java/chap11-io-lecture-source/src/main/java/com/ohgiraffers/section03/filterstream/Application2.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.section03.filterstream;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+        /* 수업목표. 표준입출력(콘솔을 통한 입출력)을 이해하고 활용할 수 있다. */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("문자열 입력: ");
+        try {
+            String input = br.readLine();
+            System.out.println("input = " + input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
