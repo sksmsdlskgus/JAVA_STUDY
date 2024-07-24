e38b89bbbd610de3262f2f34a5778f3b9cc5e82c
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-07-24 09:24:08 +0900

    Queue

---
+++ 01_java/chap13-collection-lecture-source/src/main/java/com/ohgiraffers/section01/list/run/Application5.java
@@ -0,0 +1,46 @@
+package com.ohgiraffers.section01.list.run;
+
+import java.util.*;
+
+public class Application5 {
+    public static void main(String[] args) {
+
+        /* 수업목표. Queue에 대해 이해하고 활용할 수 있다.*/
+        /*  필기.
+         *    Queue란?
+         *    선형 메모리 공간에 데이터를 저장하여 순서를 위한 선입선출(FIFO) 방식의 자료구조이다.
+         *    대부분 LinkedList를 많이 사용한다.
+         * */
+
+//        Queue queue = new LinkedList();
+//        Queue<String> que = new LinkedList<>();
+//        LinkedList<String> que = new LinkedList<>();
+        // LinkedList는 queue 중에서도 deque에 해당되어
+        // offerFirst()나 offerLast()같은 메소드도 제공된다.
+
+        /* 설명. PriorityQueue를 활용하면 선입선출 + 정렬의 개념을 가져갈 수 있다. */
+//        PriorityQueue<String> que = new PriorityQueue<>(); // String의 기준대로 문자열 오름차순
+        PriorityQueue<String> que = new PriorityQueue<>(Collections.reverseOrder()); // 반대인 내림차순
+
+
+        que.offer("first");
+        que.offer("second");
+        que.offer("third");
+        que.offer("fourth");
+        que.offer("fifth");
+
+        System.out.println("que = " + que);
+        System.out.println("peek(): " + que.peek());
+
+        System.out.println("que = " + que);
+        System.out.println("poll(): " + que.poll());
+        System.out.println("que = " + que);
+        System.out.println("poll(): " + que.poll());
+        System.out.println("que = " + que);
+        System.out.println("poll(): " + que.poll());
+        System.out.println("que = " + que);
+        System.out.println("poll(): " + que.poll());
+        System.out.println("que = " + que);
+
+    }
+}
