5612158c71fe898139b1f2523dacf1d55b6cc5e7
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-07-29 12:20:15 +0900

    stream

--- 01_java/chap16-steam-lecture-source/src/main/java/com/ohgiraffers/section02/uses/subsection03/terminal/Application2.java
+++ 01_java/chap16-steam-lecture-source/src/main/java/com/ohgiraffers/section02/uses/subsection03/terminal/Application2.java
@@ -22,9 +22,12 @@ public class Application2 {
         System.out.println("reduceTwoParam = " + reduceTwoParam);
 
         /* 설명. 인자가 3개일 경우 */
+        /* 설명. 매개변수 3번째는 좀더 효율적인 가산 누적연산을 위한 중간 합계 처리용 람다식을 작성한다. (2번째 인자의 결과와 호환이 가능해야 한다.) */
         Integer reduceThreParam = Stream.of(1,2,3,4,5,6,7,8,9,10)
                 .reduce(100,Integer::sum,(x,y) -> x + y);
 
         System.out.println("reduceThreParam = " + reduceThreParam);
     }
 }
+
+/* 필기. 오 키보드 바꼈구나!!! 불빛도 삐까뻔쩍하군!!! ㅋㅋㅋㅋㅋㅋ*/
