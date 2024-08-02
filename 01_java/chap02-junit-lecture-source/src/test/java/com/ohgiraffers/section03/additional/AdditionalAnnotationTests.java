fb6cd3dcf46d954b56c4d230d67496096205d7e1
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-02 10:39:19 +0900

    TDD

---
+++ 01_java/chap02-junit-lecture-source/src/test/java/com/ohgiraffers/section03/additional/AdditionalAnnotationTests.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.section03.additional;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
+public class AdditionalAnnotationTests {
+
+    /* 수업목표. Junit5에서 제공하는 메소드 관련 추가 어노테이션을 사용할 수 있다. */
+   @Disabled //단위테스트를 블록 상태로 만들자.
+   @Test
+    public void testIgnore() {
+
+    }
+
+    @Test
+    @Timeout(value = 1000, unit = TimeUnit.MILLISECONDS)
+    public void testTimeout() throws InterruptedException {
+       Thread.sleep(900);  // 통과
+//       Thread.sleep(1000);  // 예외발생
+
+    }
+
+    @Test
+    @Order(1)   //작을수록 우선순위가 높다.(먼저 실행된다.)
+    public void testFirst(){}
+
+    @Test
+    @Order(3)
+    public void testThird(){}
+
+    @Test
+    @Order(2)
+    public void testSecond(){}
+
+
+
+}
