4254cfe9592cdcd875b0fd5be875e0c8a43ed317
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-02 12:08:46 +0900

    TDD

--- 01_java/chap03-parameterized-test-lecture-source/src/test/java/com/ohgiraffers/parameterized/section01/params/ParameterizedTests.java
+++ 01_java/chap03-parameterized-test-lecture-source/src/test/java/com/ohgiraffers/parameterized/section01/params/ParameterizedTests.java
@@ -11,18 +11,19 @@ import java.util.stream.Stream;
 
 public class ParameterizedTests {
 
-    /* 목차. 1. @ValueSource를 이용한 parameter value 목록 지정 */
+    /* 목차. 1. @ ValueSource를 이용한 parameter value 목록 지정 */
     @DisplayName("홀수 짝수 판별 테스트")
     @ParameterizedTest
     @ValueSource(ints = {1, 3, -1, 15, 123})
-    void testIsOdd(int number) {
+    void testIdOdd(int number) {
 
-        // given (ValueSoure 자체가 given이다.
+        // given (ValueSoure 자체가 given 이다.)
         // when
         boolean result = NumberValidator.isOdd(number);
 
         // then
         Assertions.assertTrue(result);
+
     }
 
     @DisplayName("null값 테스트")
@@ -35,6 +36,7 @@ public class ParameterizedTests {
 
         // then
         Assertions.assertTrue(result);
+
     }
 
     @DisplayName("empty값 테스트")
@@ -47,6 +49,7 @@ public class ParameterizedTests {
 
         // then
         Assertions.assertTrue(result);
+
     }
 
     @DisplayName("blank값 테스트")
@@ -59,16 +62,16 @@ public class ParameterizedTests {
 
         // then
         Assertions.assertTrue(result);
+
     }
 
     @DisplayName("Month에 정의 된 타입들이 1~12월 사이의 범위인지 테스트")
     @ParameterizedTest
     @EnumSource(Month.class)
-    void testMonthValueCollect(Month month) {
-        System.out.println("month = " + month);
-        boolean result = DateValidator.isCollect(month);
-
+    void testIsMonthValueCollect(Month month) {
+        boolean result = DataValidator.isCollect(month);
         Assertions.assertTrue(result);
+
     }
 
     @DisplayName("4월, 6월, 9월, 11월이 30일인지 확인")
@@ -78,30 +81,30 @@ public class ParameterizedTests {
 
         int verifyValue = 30;
 
-        int actual = DateValidator.getLastDayOf(month);
+        int actual = DataValidator.getLastDatOf(month);
+        Assertions.assertEquals(verifyValue,actual);
 
-        Assertions.assertEquals(verifyValue, actual);
     }
 
     @DisplayName("2월, 4월, 6월, 9월, 11월을 제외한 달이 31일인지 확인")
     @ParameterizedTest
-    @EnumSource(
-            value = Month.class,
-            names = {"FEBRUARY", "APRIL", "JUNE", "SEPTEMBER", "NOVEMBER"},
+    @EnumSource(value = Month.class,
+            names = {"FEBRUARY","APRIL", "JUNE", "SEPTEMBER", "NOVEMBER"},
             mode = EnumSource.Mode.EXCLUDE
     )
     void testHasThirtyOneDaysLong(Month month) {
 
         int verifyValue = 31;
 
-        int actual = DateValidator.getLastDayOf(month);
+        int actual = DataValidator.getLastDatOf(month);
+        Assertions.assertEquals(verifyValue,actual);
 
-        Assertions.assertEquals(verifyValue, actual);
     }
+
 
     @DisplayName("영문자를 대문자로 변경하는지 확인")
     @ParameterizedTest
-    @CsvSource(         // String 형태로 두개의 매개변수를 각각 넘겨주고 싶다면
+    @CsvSource(    // String 형태로 두개의 매개변수를 각각 넘겨주고 싶다면
             value = {"test:TEST", "tEst: TEST", "JavaScript: JAVASCRIPT"},
             delimiter = ':'
     )
@@ -110,29 +113,27 @@ public class ParameterizedTests {
         System.out.println("verifyValue = " + verifyValue);
 
         String actual = input.toUpperCase();
-
-        Assertions.assertEquals(verifyValue, actual);
+        Assertions.assertEquals(verifyValue,actual);
     }
 
     @DisplayName("CSV 파일을 읽은 테스트 데이터를 테스트에 활용하는지 확인")
     @ParameterizedTest
 
     /* 설명. numLinesToSkip은 파일을 읽어들일 때 위에서 부터 무시할 라인 수를 입력 */
-    @CsvFileSource(resources = "/parameter-test-data.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "/parameter-test-data.csv", numLinesToSkip = 2)
     void testToUpperCaseWithCSVFileData(String input, String verifyValue) {
         System.out.println("input = " + input);
         System.out.println("verifyValue = " + verifyValue);
 
         String actual = input.toUpperCase();
-
         Assertions.assertEquals(verifyValue, actual);
     }
 
-    private static Stream<Arguments> providerStringSource() {
+    public static Stream<Arguments> providerStringSource(){
         return Stream.of(
                 Arguments.of("hello world", "HELLO WORLD"),
                 Arguments.of("JavaScript", "JAVASCRIPT"),
-                Arguments.of("tEsT", "TEST")
+                Arguments.of("tEst", "TEST")
         );
     }
 
@@ -154,4 +155,5 @@ public class ParameterizedTests {
 
         Assertions.assertEquals(verifyValue, actual);
     }
+
 }
