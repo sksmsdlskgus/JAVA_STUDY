d5e455b33d657839914947cd4c59993955ab3735
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-02 15:41:29 +0900

    Jdbc 조회

--- 02_jdbc/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section01/statement/Application1.java
+++ 02_jdbc/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section01/statement/Application1.java
@@ -10,11 +10,16 @@ import static com.ohgiraffers.common.JDBCTemplate.getConnection;
 
 public class Application1 {
     public static void main(String[] args) {
+
+
+        /* 설명. 트랜젝션 처리를 위한 DBMS 연동용 Connection 객채 샹성*/
         Connection con = getConnection();
 
         System.out.println("con = " + con);
-        Statement stmt = null;
-        ResultSet rset = null;        //조회를 할 예정
+
+        /* 설명. 해당 Connection을 통해 트랜젝션 처리(비지니스 로직 처리, CRUD 등) */
+        Statement stmt = null;         // 쿼리를 운반하고 결과를 반환
+        ResultSet rset = null;        //조회를 할 예정(DML 작업이면 ResultSet 대신 int로 처리)
 
         try {
             stmt = con.createStatement();
