27f95e172f12717424c92574d0132c3022510ca6
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-04 09:36:17 +0900

    JDBC

--- 02_jdbc/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section02/prepared/Application2.java
+++ 02_jdbc/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/section02/prepared/Application2.java
@@ -19,12 +19,15 @@ public class Application2 {
         System.out.print("사번을 입력하세요: ");
         String empId = sc.nextLine();
 
-        String query = "SELECT EMP_ID, EMP_NAME FROM EMPLOYEE WHERE EMP_ID = ?" ;
+        String entYn = "N";
+
+        String query = "SELECT EMP_ID, EMP_NAME FROM EMPLOYEE WHERE EMP_ID = ? AND ENT_YN = ?";
         //?는 placeholder이라고 하며, 아래의  pstmt.setString(1, empId); 를 받는다는것을 의미함
 
         try {
             pstmt = con.prepareStatement(query);
             pstmt.setString(1, empId);
+            pstmt.setString(2, entYn);
 
             rset = pstmt.executeQuery();
             if (rset.next()){
