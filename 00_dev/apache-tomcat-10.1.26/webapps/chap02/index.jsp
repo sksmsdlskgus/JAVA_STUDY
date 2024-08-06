1accfa318974776830fb3391ec09816ba630a1c1
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-06 11:23:28 +0900

    servlet Post 요청

--- 00_dev/apache-tomcat-10.1.26/webapps/chap02/index.jsp
+++ 00_dev/apache-tomcat-10.1.26/webapps/chap02/index.jsp
@@ -6,8 +6,17 @@
 </head>
 <body>
     <h1 align="center">Service Method</h1>
-    <h3>Get 방식의 요청</h3>
+    <h3>GET 방식의 요청</h3>
     <h4>a 태그의 href 속성값 변경</h4>
     <a href="request-service">서비스 메소드 요청하기</a>
+
+    <h3>POST 방식의 요청</h3>
+    <h4>from 태그의 method 속성을 post로 설정</h4>
+    <form action ="request-service" method = "get">
+        <input type="text" name="number" value="123">
+        <input type="text" name="number" value="홍길동">
+        <input type="submit" value="POST 방식 요청 전송">
+    </form>
+
 </body>
 </html>
--- 03_servlet/chap02-service-method-lecture-source/src/main/java/com/ohgiraffers/section01/servicemethod/ServiceMethodTestServlet.java
+++ 03_servlet/chap02-service-method-lecture-source/src/main/java/com/ohgiraffers/section01/servicemethod/ServiceMethodTestServlet.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 @WebServlet("/request-service")
 public class ServiceMethodTestServlet extends HttpServlet {
 
+
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         System.out.println("req = " + req);
@@ -25,6 +26,21 @@ public class ServiceMethodTestServlet extends HttpServlet {
         String httpMethod = httpquest.getMethod();
         System.out.println("요청 방식 : " + httpMethod);
 
+        if("GET".equals(httpMethod)) { // 널포인터 방지 순서 변환 조건 걸기
+            doGet(httpquest, httpsresponse);
+        } else if("POST".equals(httpMethod)) {
+            doPost(httpquest, httpsresponse);
+        }
 
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Get 요청을 처리할 메소드 호출중...");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Post 요청을 처리할 메소드 호출중...");
     }
 }
