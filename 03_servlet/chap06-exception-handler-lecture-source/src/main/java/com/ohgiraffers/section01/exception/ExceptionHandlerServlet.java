244561dc437ed2d3b063928d52e41daa0c5864c3
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-06 15:31:03 +0900

    servlet errorpage 이해

---
+++ 03_servlet/chap06-exception-handler-lecture-source/src/main/java/com/ohgiraffers/section01/exception/ExceptionHandlerServlet.java
@@ -0,0 +1,45 @@
+package com.ohgiraffers.section01.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+@WebServlet("/showErrorPage")
+public class ExceptionHandlerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Enumeration<String> attrNames = req.getAttributeNames();
+        while (attrNames.hasMoreElements()) {
+            System.out.println(attrNames.nextElement());
+        }
+
+        int statusCode = (int) req.getAttribute("jakarta.servlet.error.status_code");
+        String message = (String) req.getAttribute("jakarta.servlet.error.message");
+        String servletName = (String) req.getAttribute("jakarta.servlet.error.servlet_name");
+
+
+        StringBuilder errorPage = new StringBuilder();
+        errorPage.append("<h1 align=\"center\">")
+                .append(statusCode)
+                .append("-")
+                .append(message)
+                .append("<br>\n")
+                .append("<p>에러 발생한 서블릿 명: ")
+                .append(servletName)
+                .append("</p>\n")
+                .append("</h1>\n");
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.print(errorPage);
+        out.flush();
+        out.close();
+    }
+}
---
+++ 03_servlet/chap06-exception-handler-lecture-source/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JSP - Hello World</title>
+</head>
+<body>
+<h1 align="center">Exception Handler</h1>
+<ul>
+  <li><a href="show404error">404에러 확인</a></li>
+  <li><a href="show500error">500에러 확인</a></li>
+</ul>
+</body>
+</html>
