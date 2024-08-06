d60c0c9b11bc9344a9fc931ffeac874c14f5ab82
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-06 12:38:30 +0900

    servlet

---
+++ 03_servlet/chap04-request-lecture-source/src/main/java/com/ohgiraffers/header/RequestHeaderPrintServlet.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.header;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet("/headers")
+public class RequestHeaderPrintServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            System.out.println(headerNames.nextElement());
+        }
+    }
+}
