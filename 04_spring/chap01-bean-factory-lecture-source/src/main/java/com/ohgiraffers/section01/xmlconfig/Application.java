0207c091e6b46d63789d35be9dcabcb89d725ac0
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-08 11:48:44 +0900

    spring Bean xml 방식

---
+++ 04_spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+import com.ohgiraffers.common.MemberDTO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context = new GenericXmlApplicationContext("section02/xmlconfig/spring-context.xml");
+        //xml파일에서 bean설정 등록한것을 가져올 경로 설정해서 콩의 객체를 출력해보자
+
+        MemberDTO member2 = (MemberDTO) context.getBean("member2");
+        System.out.println("member2 = " + member2);
+        //member2 = MemberDTO(sequence=2, id=user02, pwd=pass01, name=이나짱)
+
+    }
+}
