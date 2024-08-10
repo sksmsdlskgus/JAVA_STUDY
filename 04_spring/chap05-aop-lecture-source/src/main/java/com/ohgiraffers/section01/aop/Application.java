c3ef263b7eb4f8ba7fae4ee94bc8a6189599339b
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-10 09:28:26 +0900

    aop start

--- 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/Application.java
+++ 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/Application.java
@@ -1,4 +1,11 @@
 package com.ohgiraffers.section01.aop;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.ohgiraffers.section01.aop");
+        
+    }
 }
