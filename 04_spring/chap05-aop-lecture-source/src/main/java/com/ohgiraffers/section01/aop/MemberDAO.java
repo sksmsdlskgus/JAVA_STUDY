fa955246207c2667d53f1cbb00c17c57abadb1b7
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-11 09:30:45 +0900

    aop refactor

---
+++ 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/MemberDAO.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.section01.aop;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDAO {
+}
---
+++ 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/MemberService.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.section01.aop;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+}
