689453d8aab9dc106406978b33842f13a641d4e5
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-12 09:34:19 +0900

    aop 생성자 주입

--- 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/Application.java
+++ 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/Application.java
@@ -3,9 +3,15 @@ package com.ohgiraffers.section01.aop;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.ohgiraffers.section01.aop");
-        
+
+        MemberService memberService = context.getBean("memberService", MemberService.class);
+        System.out.println("========= select all members ========");
+
+        List<MemberDTO> members = memberService.findMembers();
     }
 }
--- 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/MemberService.java
+++ 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/MemberService.java
@@ -1,7 +1,23 @@
 package com.ohgiraffers.section01.aop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
+
+
+    private final MemberDAO memberDAO;
+
+    @Autowired
+    public MemberService(MemberDAO memberDAO) { // final이 있으니까 이런식으로 생성자 주입
+        this.memberDAO = memberDAO;
+    }
+
+    public List<MemberDTO> findMembers() {
+        System.out.println("target -> findAllMembers 실행");
+        return memberDAO.selectAllMembers();
+    }
 }
