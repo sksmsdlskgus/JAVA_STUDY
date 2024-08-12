e5043041e0d4e47a0557d309eec0caa2a33e7c34
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-12 09:36:48 +0900

    aop dao bean 생성

--- 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/MemberDAO.java
+++ 04_spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/MemberDAO.java
@@ -2,6 +2,23 @@ package com.ohgiraffers.section01.aop;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class MemberDAO {
+
+    private final List<MemberDTO> memberList;
+
+    public MemberDAO() {
+        memberList = new ArrayList<>();
+        memberList.add(new MemberDTO(1L,"이나짱"));
+        memberList.add(new MemberDTO(2L,"현나이"));
+        
+    }
+
+    public List<MemberDTO> selectMembers() {
+        return null;
+    }
+
 }
