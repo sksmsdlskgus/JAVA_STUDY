75498fe82725f60c7b2eef2e1c2dd9b4e2e059a5
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-12 15:12:32 +0900

    REST API

--- 05_springboot/chap02-request-mapping-lecture-source/src/main/java/com/ohgiraffers/chap02/MethodMappingTestController.java
+++ 05_springboot/chap02-request-mapping-lecture-source/src/main/java/com/ohgiraffers/chap02/MethodMappingTestController.java
@@ -3,6 +3,8 @@ package com.ohgiraffers.chap02;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -29,5 +31,18 @@ public class MethodMappingTestController {
         model.addAttribute("message","POST 방식의 메뉴 수정용 핸들러 메소드 호출함...");
         return "mappingResult";
     }
+
+    @GetMapping("/menu/delete")
+    public String getDeleteMenu(Model model) {
+        model.addAttribute("message","GET 방식의 메뉴 삭제용 핸들러 메소드 호출함...");
+        return "mappingResult";
+    }
+
+    @PostMapping("/menu/delete")
+    public String postDeleteMenu(Model model) {
+        model.addAttribute("message","POST 방식의 메뉴 삭제용 핸들러 메소드 호출함...");
+        return "mappingResult";
+    }
+
 
 }
