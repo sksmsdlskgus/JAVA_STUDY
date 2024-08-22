55f86eac4131cfcd75495c003729df2fc17f1fe3
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-08-22 10:10:17 +0900

    EntityManager Table Create

---
+++ 08_jpa/chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/entity/EntityMappingTests.java
@@ -0,0 +1,55 @@
+package com.ohgiraffers.section01.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+
+
+public class EntityMappingTests {
+    private static EntityManagerFactory emf;
+    private static EntityManager em;
+
+    @BeforeAll
+    public static void initFactory() {
+        emf = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        em = emf.createEntityManager();
+    }
+
+    @Test
+    public void 테이블_만들기_테스트() {
+
+        // given
+        Member member = new Member();
+        member.setMemberNo(1);
+        member.setMemberId("user01");
+        member.setMemberPwd("pass01");
+        member.setNickname("홍길동");
+        member.setPhone("010-1234-5678");
+        member.setEmail("hong@gmail.com");
+        member.setAddress("서울시 서초구");
+        member.setEnrollDate(new java.util.Date());
+        member.setMemberRole("ROLE_MEMBER");
+        member.setStatus("Y");
+
+        // when
+
+        // then
+
+    }
+
+    @AfterEach
+    public void closeManager() {
+        em.close();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        emf.close();
+    }
+}
