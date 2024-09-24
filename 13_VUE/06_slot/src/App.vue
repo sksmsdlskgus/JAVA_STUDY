14d1f93fe2532d5c8f4a85166e04f5aa9633df91
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-09-24 11:18:57 +0900

    vue 예제

--- 13_VUE/06_slot/src/App.vue
+++ 13_VUE/06_slot/src/App.vue
@@ -1,47 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<!-- vbase-3-setup -->
 
+<!-- 
+template:
+vue는 사용자의 화면에 보여지는 부분을 내보낼 때 다음과같이 <template> 하위에 구성하여 작성한다.
+template은 다른 엘리먼트 요소를 감싸주는 역할을 수행하여 사용자의 화면에 렌더링 될 때 제거된다. 
+(하나의 컴포넌트가 화면에 어떻게 보여질지 컴포넌트 단위의 html이 작성된다고 볼 수 있다.)
+-->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+      <p>Hello!</p>
+      <MyComponent/>
+  </div>
 </template>
 
+<!-- 
+script setup:
+해당 컴포넌트에 적용할 setup함수 안의내용 (반응형 변수/반응형 변수에 변화를 주는 함수/라이프 사이클 훅)을 작성하며 
+view model의 개념을 작성하는 영역이다.
+-->
+
+<script setup>
+  import MyComponent from './components/01_component/MyComponent.vue'
+</script>
+
+
+<!-- 
+style scoped:
+현재 컴포넌트에서만 적용되는 스타일을 작성한다. (범위 css(고유한 번호의 컴포넌틐에만 적용됨))
+-->
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ 13_VUE/06_slot/src/components/01_component/MyComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>MyComponent</h1>
+        <p>{{onMounted}}</p>
+    </div>
+</template>
+
+<script setup>
+ import {onMounted} from 'vue';
+
+// export default{
+//     setup(){
+        onMounted(async() => {
+            console.log("Hello world!");
+        })
+
+    // }
+// }
+
+</script>
+
+<style scoped>
+
+</style>
