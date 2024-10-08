362a8672c76ca1a53123750cf5944345f4371687
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-10-08 09:30:26 +0900

    docker 와 cors 연결하기

--- 14_devops/chap01_01_bootProject/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
+++ 14_devops/chap01_01_bootProject/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
+//@Configuration
+//@EnableWebMvc // 이거 두개 주석처리를 한다면 포조 형태로 변함
 public class WebConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) { //서블릿 기반에서만 작동하고 , 게이트 웨이에서는 안먹힌다.
--- 14_devops/chap01_02_vueProject/src/App.vue
+++ 14_devops/chap01_02_vueProject/src/App.vue
@@ -17,8 +17,14 @@ const num2 = ref(0);
 const result = ref(0);
 
 const sendPlus = async() => {
-  const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`);
+  // const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`);
   // const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
+  
+  // 8055 컨테이너 (8055:7777로 된 백엔드 서버 컨테이너)로 요청 시 (백엔드 에서 cors처리를 했을 경우)
+  // const response = await fetch(`http://localhost:8055/api/plus?num1=${num1.value}&num2=${num2.value}`);
+
+  // 프론트에서 cors처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너가 아닌 상태)
+  const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
   const data = await response.json();
   result.value = data.sum;
 }
