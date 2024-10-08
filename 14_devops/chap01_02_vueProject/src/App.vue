b41f09f987828a287d4d56b8695f1fc32f168b47
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-10-08 11:13:41 +0900

    docker 와 cors 연결하기

--- 14_devops/chap01_02_vueProject/src/App.vue
+++ 14_devops/chap01_02_vueProject/src/App.vue
@@ -1,39 +1,45 @@
+<!-- vbase-3-setup -->
 <template>
-  <div class="pluss">
-<h1>덧셈 기능 만들기</h1>
-<label> num1: </label><input type="text" v-model="num1">&nbsp;
-<label> num2: </label><input type="text" v-model="num2">&nbsp;
-  <button @click="sendPlus">더하기</button>
-  <hr>
-  <p>`{{ num1 }} + {{ num2 }} = {{ result }}`</p>
+  <div class="plus">
+    <h1>덧셈 기능 만들기</h1>
+    <label>num1: </label><input type="text" v-model="num1">&nbsp;
+    <label>num2: </label><input type="text" v-model="num2">&nbsp;
+    <button @click="sendPlus">더하기</button>
+    <hr>
+    <p>`{{ num1 }} + {{ num2 }} = {{ result }}`</p>
   </div>
 </template>
 
 <script setup>
-import {ref} from 'vue';
+  import { ref } from 'vue';
 
-const num1 = ref(0);
-const num2 = ref(0);
-const result = ref(0);
+  const num1 = ref(0);
+  const num2 = ref(0);
+  const result = ref(0);
 
-const sendPlus = async() => {
-  // const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`);
-  // const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
-  
-  // 8055 컨테이너 (8055:7777로 된 백엔드 서버 컨테이너)로 요청 시 (백엔드 에서 cors처리를 했을 경우)
-  // const response = await fetch(`http://localhost:8055/api/plus?num1=${num1.value}&num2=${num2.value}`);
+  const sendPlus = async() => {
+    // const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`);
+    // const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
 
-  // 프론트에서 cors처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너가 아닌 상태)
-  const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
-  const data = await response.json();
-  result.value = data.sum;
-}
+    /* 8055컨테이너(8055:7777로 된 백엔드 서버 컨테이너)로 요청 시(백엔드에서 CORS 처리를 했을 경우) */
+    // const response = await fetch(`http://localhost:8055/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    /* 프론트에서 CORS처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너가 아닌 상태) */
+    // const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    /* 프론트에서 CORS처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너인 상태) */
+    const response = await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    const data = await response.json();
+    result.value = data.sum;
+  }
+
+
 
 </script>
 
 <style scoped>
-.plus{
-  text-align: center;
-}
-
+  .plus {
+    text-align: center;
+  }
 </style>
--- 14_devops/chap01_02_vueProject/vite.config.js
+++ 14_devops/chap01_02_vueProject/vite.config.js
@@ -16,7 +16,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8055',
+        // target: 'http://localhost:7777',
+
+        /* 프론트에서 CORS처리를 하고자 하는 경우(프론트 프로젝트가 컨테이너가 아닌 상태) */
+        // target: 'http://localhost:8055',
+
+        /* docker-compose 및 docker network를 활용해 'springboot-app'이라는 이름으로 통신할 경우(프론트 프로젝트가 컨테이너인 경우) */
+        /* 백엔드 컨테이너 내부에서 동작하는 포트 번호를 작성해야 한다.(8055:7777) */
+        target: 'http://springboot-app:7777',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
